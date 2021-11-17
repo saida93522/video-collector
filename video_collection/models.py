6a84f912e6d9e8a5929971d34dfbd4710964643b
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-11-17 10:47:04 -0600

    rebuild add form

--- video_collection/models.py
+++ video_collection/models.py
@@ -7,7 +7,36 @@ class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True) # https://www.youtube.com/watch?v=video_id
     objects = models.Manager()
+
+    
+    def save(self, *args, **kwargs):
+        """ overrides save method and verrifies youtube url is valid before saved to db.Extracts the video id from the URL if valid."""
+        try:
+            url_components = parse.urlparse(self.url)
+
+            if url_components.scheme != 'https':
+                raise ValidationError(f'Not a YouTube URL {self.url}')
+
+            if url_components.netloc != 'www.youtube.com':
+                raise ValidationError(f'Not a YouTube URL {self.url}')
+                
+            if url_components.path != '/watch':
+                raise ValidationError(f'Not a YouTube URL {self.url}')
+            
+            query_string = url_components.query
+            if not query_string:
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+            parameters = parse.parse_qs(query_string, strict_parsing=True) #converts video_id to dict = {'v_id':45344}
+            v_parameter_list = parameters.get('v')
+            if not v_parameter_list:   # rais error if v=non or an empty list
+                raise ValidationError(f'Invalid YouTube URL parameters {self.url}')
+            self.video_id = v_parameter_list[0]   # set the video_id
+        except ValueError as e:  
+            raise ValidationError(f'Unable to parse URL {self.url}') from e
+
+        super().save(*args, **kwargs)  
 
    
                     
@@ -19,4 +48,4 @@ class Video(models.Model):
         else:
             notes = self.notes[1:200]
         return f'ID: {self.pk}, Name: {self.name}, URL: {self.url},  \
-        Video ID:,  Notes: {notes}'
+        Video ID{self.video_id}:,  Notes: {notes}'
--- video_collection/views.py
+++ video_collection/views.py
@@ -14,27 +14,5 @@ def home(request):
 
 def add(request):
     """ add and verify videos using form model objects and save to database. """
-    if request.method == 'POST':  
-        new_video_form = VideoForm(request.POST) #form now equals the data requested with
-        if new_video_form.is_valid(): #verify that the data matches
-            try:
-                new_video_form.save()  # Creates new Video object and saves 
-                return redirect('video_list')
-            except IntegrityError:
-                messages.warning(request, 'You already added that video')
-            except ValidationError:
-                messages.warning(request, 'Invalid YouTube URL')
-        
-        # Invalid form 
-        messages.warning(request, 'Check the data entered')
-        context = {'new_video_form': new_video_form}
-        return render(request, 'video_collection/add.html',context) 
-            
-        
-    new_video_form = VideoForm()
-    context = {'new_video_form':new_video_form}
     return render(request, 'video_collection/add.html', context) 
-    
-
-
 
